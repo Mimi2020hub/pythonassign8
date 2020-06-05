dc8d677278f5b404bf37a12c84e56c3222ce8570
Mimi2020hub <63885221+Mimi2020hub@users.noreply.github.com>
2020-06-05 11:37:02 -0700

    Update forms.py

--- forms.py
+++ forms.py
@@ -4,19 +4,19 @@ from .models import Meeting, MeetingMinutes, Resource, Event
 class MeetingForm(forms.ModelForm):
     class Meta:
         model=Meeting
-        fields = '__all__'_''
+        fields = '__all__'
 
 class MeetingMinutesForm(forms.ModelForm):
     class Meta:
         model=MeetingMinutes
-        fields = '__all__'_''
+        fields = '__all__'
 
 class ResourceForm(forms.ModelForm):
     class Meta:
         model=Resource
-        fields = '__all__'_''
+        fields = '__all__'
 
 class EventForm(forms.ModelForm):
     class Meta:
         model=Event
-        fields = '__all__'_''
+        fields = '__all__'
